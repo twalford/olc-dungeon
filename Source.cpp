c1262d3a0f453364ce4d112bda80c5170eba74fd
Thomas Walford <20815153+twalford@users.noreply.github.com>
2018-06-09 15:50:59 +1000

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+#include "TArray.h"
+#include "olcConsoleGameEngine.h";
+using namespace std;
+
+#pragma region
+enum compass_directions { NORTH = 0, EAST, SOUTH, WEST };
+enum entity_types { EMPTY = 0, FLOOR, WALL, ICE, FIRE, HERO };
+
+typedef entity_types ENTITY;
+
+typedef struct _tile
+{
+	ENTITY type;
+} TILE, *LPTILE;
+
+typedef compass_directions DIRECTION;
+typedef TArray2D<TILE> GAMEBOARD;
+#pragma endregion GAME TYPES
+
+class Dungeon : public olcConsoleGameEngine
+{
+public:
+	Dungeon()
+	{
+
+	}
+
+private:
+	GAMEBOARD m_xBoard;
+
+	//Sprites
+	olcSprite *spriteHERO	= nullptr;
+	olcSprite *spriteFLOOR	= nullptr;
+	olcSprite *spriteICE	= nullptr;
+	olcSprite *spriteFIRE	= nullptr;
+	olcSprite *spriteEMPTY	= nullptr;
+
+	const short TILE_SIZE = 8;
+
+	pair<short, short> m_sPos;
+	pair<short, short> m_sBufOffset;
+
+	void ShiftBoard(DIRECTION d)
+	{
+		switch (d)
+		{
+			// Shift board down (player goes north)
+		case NORTH:
+			for (int r = m_xBoard.rows() - 1; r >= 0; r--)
+			{
+				for (int c = m_xBoard.cols() - 1; c >= 0; c--)
+				{
+					if (r > 0)
+						m_xBoard(r, c).type = m_xBoard(r - 1, c).type;
+					else
+						m_xBoard(r, c).type = EMPTY;
+				}
+			}
+			break;
+			
+			// Shift board up (player goes south)
+		case SOUTH:
+			for (int r = 0; r < m_xBoard.rows(); r++)
+			{
+				for (int c = 0; c < m_xBoard.cols(); c++)
+				{
+					if (r < m_xBoard.rows() - 1)
+						m_xBoard(r, c).type = m_xBoard(r + 1, c).type;
+					else
+						m_xBoard(r, c).type = EMPTY;
+				}
+			}
+
+			break;
+		default:
+			break;
+		}
+	}
+	void UpdateScreen()
+	{
+		for (int r = 0; r < m_xBoard.rows() - m_sBufOffset.second; r++)
+		{
+			for (int c = 0; c < m_xBoard.cols(); c++)
+			{
+				// The TArray implementation is diagonally flipped,
+				// so account for it when converting to OLC buffer.
+				// [r,c] becomes [c,r]
+				switch (m_xBoard(r + m_sBufOffset.second, c).type)
+				{
+				case EMPTY:	DrawSprite(c * TILE_SIZE, r * TILE_SIZE, spriteEMPTY);	break;	   
+				case HERO:	DrawSprite(c * TILE_SIZE, r * TILE_SIZE, spriteHERO);	break;
+				case FLOOR:	DrawSprite(c * TILE_SIZE, r * TILE_SIZE, spriteFLOOR);	break;	   
+				case ICE:	DrawSprite(c * TILE_SIZE, r * TILE_SIZE, spriteICE);	break;	   
+				case FIRE:	DrawSprite(c * TILE_SIZE, r * TILE_SIZE, spriteFIRE);	break;	   
+				default:	   
+					DrawSprite(c * TILE_SIZE, r * TILE_SIZE, spriteEMPTY);
+					break;
+				}
+			}
+		}
+	}
+
+	void MovePlayer(DIRECTION d)
+	{
+		// Overwrite the old position with floor.
+		m_xBoard(m_sPos.first, m_sPos.second).type = FLOOR;
+
+		switch (d)
+		{
+		case NORTH: m_sPos.first  -= 1; break;
+		case EAST:  m_sPos.second += 1;	break;
+		case SOUTH: m_sPos.first  += 1; break;
+		case WEST:  m_sPos.second -= 1;	break;
+		}
+
+		//stay in boundry
+		if (m_sPos.first < 0) m_sPos.first = 0; //technically shouldnt need this
+		else if (m_sPos.second < 0) m_sPos.second = 0;
+		else if (m_sPos.first  > m_xBoard.rows() - 1) m_sPos.first  = m_xBoard.rows() - 1; //technically shouldnt need this either
+		else if (m_sPos.second > m_xBoard.cols() - 1) m_sPos.second = m_xBoard.cols() - 1;
+
+		// save the new position.
+		m_xBoard(m_sPos.first, m_sPos.second).type = HERO;
+
+		//Payers row position should never change.
+		m_sPos.first = 21;
+		//ShiftBoard
+		ShiftBoard(d);
+
+		//Update screen
+		UpdateScreen();
+	}
+
+protected:
+	virtual bool OnUserCreate() 
+	{
+		spriteHERO		= new olcSprite(L"../OLC Sprite Editor/dungeon_hero.spr");
+		spriteFLOOR		= new olcSprite(L"../OLC Sprite Editor/dungeon_floor.spr");
+		spriteICE		= new olcSprite(L"../OLC Sprite Editor/dungeon_ice.spr");
+		spriteFIRE		= new olcSprite(L"../OLC Sprite Editor/dungeon_fire.spr");
+		spriteEMPTY		= new olcSprite(L"../OLC Sprite Editor/dungeon_empty.spr");
+
+		m_sPos.first = 21;
+		m_sPos.second = 7;
+		
+		m_sBufOffset.first = 0;
+		m_sBufOffset.second = 8;
+
+		m_xBoard.resize(30, 15);
+
+		//Make everything empty
+		for (int i = 0; i < m_xBoard.rows(); i++)
+			for (int j = 0; j < m_xBoard.cols(); j++)
+				m_xBoard(i, j).type = FLOOR;
+
+		m_xBoard(m_sPos.first, m_sPos.second).type = HERO;
+
+		//m_xBoard(0, 1).type = FIRE;
+		//m_xBoard(2, 1).type = EMPTY;
+		//m_xBoard(3, 1).type = ICE;
+		//m_xBoard(5, 1).type = FIRE;
+
+#pragma region 
+		
+		// I
+		m_xBoard(1, 11).type = ICE;
+		m_xBoard(1, 12).type = ICE;
+		m_xBoard(1, 13).type = ICE;
+		m_xBoard(2, 12).type = ICE;
+		m_xBoard(3, 12).type = ICE;
+		m_xBoard(4, 12).type = ICE;
+		m_xBoard(5, 11).type = ICE;
+		m_xBoard(5, 12).type = ICE;
+		m_xBoard(5, 13).type = ICE;
+		// N 
+		
+		m_xBoard(7, 11).type = FIRE;
+		m_xBoard(7, 12).type = FIRE;
+		m_xBoard(7, 13).type = FIRE;
+		m_xBoard(8, 11).type = FIRE;
+		m_xBoard(8, 13).type = FIRE;
+		m_xBoard(9, 11).type = FIRE;
+		m_xBoard(9, 13).type = FIRE;
+		m_xBoard(10, 11).type = FIRE;
+		m_xBoard(10, 13).type = FIRE;
+		
+		// D
+		m_xBoard(14, 11).type = ICE;
+		m_xBoard(14, 12).type = ICE;
+		m_xBoard(15, 11).type = ICE;
+		m_xBoard(15, 13).type = ICE;
+		m_xBoard(16, 11).type = ICE;
+		m_xBoard(16, 13).type = ICE;
+		m_xBoard(17, 11).type = ICE;
+		m_xBoard(17, 13).type = ICE;
+		m_xBoard(18, 11).type = ICE;
+		m_xBoard(18, 12).type = ICE;
+		// E
+		m_xBoard(20, 11).type = FIRE;
+		m_xBoard(20, 12).type = FIRE;
+		m_xBoard(20, 13).type = FIRE;
+		m_xBoard(21, 11).type = FIRE;
+		m_xBoard(22, 11).type = FIRE;
+		m_xBoard(22, 12).type = FIRE;
+		m_xBoard(22, 13).type = FIRE;
+		m_xBoard(23, 11).type = FIRE;
+		m_xBoard(24, 11).type = FIRE;
+		m_xBoard(24, 12).type = FIRE;
+		m_xBoard(24, 13).type = FIRE;
+		// V
+		m_xBoard(26, 11).type = ICE;
+		m_xBoard(26, 13).type = ICE;
+		m_xBoard(27, 11).type = ICE;
+		m_xBoard(27, 13).type = ICE;
+		m_xBoard(28, 12).type = ICE;
+		
+#pragma endregion DEV LEVEL TILES
+		
+		UpdateScreen();
+
+		return true;
+	}
+	virtual bool OnUserUpdate(float fElapsedTime)
+	{
+		if (m_keys[VK_UP].bPressed) MovePlayer(NORTH);
+		else if (m_keys[VK_DOWN].bPressed) MovePlayer(SOUTH);
+		else if (m_keys[VK_LEFT].bPressed) MovePlayer(WEST);
+		else if (m_keys[VK_RIGHT].bPressed) MovePlayer(EAST);
+		
+		return true;
+	}
+};
+
+int main()
+{
+	Dungeon dungeonGame;
+	dungeonGame.ConstructConsole(120, 120, 5, 5);
+	dungeonGame.Start();
+	return 0;
+}
---
+++ TArray.h
@@ -0,0 +1,337 @@
+// * * TArray2D.h
+// * * Author: @Timkin  (thank you Tim!)
+// * * Created: 05/03/11
+
+#ifndef TARRAY2D_H
+#define TARRAY2D_H
+
+
+#include <memory>
+#include <new>
+#include <exception>
+
+#include <string.h>
+
+const size_t BUFSIZE = 256;
+
+// Visual Studio accepts but does not implement exception specifications... so stop it from complaining
+#pragma warning( disable : 4290)
+
+// define a new exception class for invalid matrix (2D) indices
+class invalid_index : public std::exception
+{
+private:
+	size_t r, c;
+	char szMsg[BUFSIZE];
+
+protected:
+	invalid_index& operator=(const invalid_index &bi) {}
+
+public:
+	invalid_index(size_t const &_r, size_t const &_c)
+		: r(_r), c(_c) {}
+
+	invalid_index(const invalid_index &bi)
+	{
+		r = bi.r;
+		c = bi.c;
+#ifdef _WIN32      
+		strcpy_s(szMsg, BUFSIZE * sizeof(char), bi.szMsg);
+#else
+		strncpy(szMsg, bi.szMsg, BUFSIZE * sizeof(char));
+#endif
+	}
+
+	const char * what()
+	{
+		// construct error msg only when needed
+#ifdef _WIN32
+		sprintf_s(szMsg, BUFSIZE * sizeof(char), "Bad index value (%u,%u)", r, c);
+#else
+		snprintf(szMsg, BUFSIZE * sizeof(char), "Bad index value (%u,%u)", r, c);
+#endif
+		return szMsg;
+	}
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	typedef struct _coord2
+	{
+		short row, col;
+
+		_coord2(short r = 0, short c = 0) : row(r), col(c) {}
+		_coord2(_coord2 const& c) : row(c.row), col(c.col) {}
+
+#ifdef _WINDOWS
+		// if _WINDOWS is defined the COORD is defined as a struct{ SHORT X,Y; } 
+		// and is used as an argument for Windows Console API functions, so
+		// we support casting to a COORD
+		operator COORD()
+		{
+			COORD c = { col,row };
+			return c;
+		}
+#endif
+
+	} COORD2, *PCOORD2;
+
+#ifdef __cplusplus
+}
+#endif
+
+
+template <typename ValueType>
+class TArray2D
+{
+
+public:
+	typedef ValueType	value_type;
+	typedef ValueType	*pointer;
+	typedef ValueType	&reference;
+	typedef ValueType	const *const_pointer;
+	typedef ValueType	const &const_reference;
+	typedef pointer			iterator;
+	typedef const_pointer	const_iterator;
+
+private:
+	size_t			m_uRows,
+		m_uCols;
+	value_type		*m_pBuffer;
+
+	void DeleteBuffer();
+	void AllocateBuffer(size_t r, size_t c);
+	size_t index_of(size_t r, size_t c);
+
+protected:
+	TArray2D(const TArray2D&) {}
+	TArray2D& operator=(const TArray2D&) {}
+
+public:
+	TArray2D();
+	TArray2D(size_t _rows, size_t _cols);
+	~TArray2D();
+
+	void resize(size_t _rows, size_t _cols);
+
+	reference   operator()(size_t row, size_t col) throw (std::bad_alloc);
+	value_type  operator()(size_t row, size_t col) const throw (std::bad_alloc);
+
+	reference   operator()(COORD2 const & p) throw (std::bad_alloc);
+	value_type  operator()(COORD2 const & p) const throw (std::bad_alloc);
+
+	iterator begin();
+	const_iterator begin() const;
+
+	iterator end();
+	const_iterator end() const;
+
+	size_t const& rows() const;
+	size_t const& cols() const;
+
+	COORD2 size() const;
+	size_t const length() const;
+
+	pointer buffer();
+	void clear();
+
+};
+
+template <typename T>
+TArray2D<T>::TArray2D()
+	: m_pBuffer(NULL), m_uRows(0), m_uCols(0)
+{
+}
+
+
+template <typename T>
+TArray2D<T>::TArray2D(size_t _rows, size_t _cols)
+	: m_pBuffer(NULL), m_uRows(_rows), m_uCols(_cols)
+{
+	AllocateBuffer(m_uRows, m_uCols);
+}
+
+
+template <typename T>
+TArray2D<T>::~TArray2D()
+{
+	DeleteBuffer();
+}
+
+
+template <typename T>
+void TArray2D<T>::DeleteBuffer()
+{
+	if (m_pBuffer)
+	{
+		delete[] m_pBuffer;
+		m_pBuffer = NULL;
+	}
+}
+
+template <typename T>
+void TArray2D<T>::AllocateBuffer(size_t r, size_t c)
+{
+	if (m_pBuffer)
+		DeleteBuffer();
+
+	if (r * c == 0)
+		throw invalid_index(r, c);
+
+	m_pBuffer = new value_type[r*c];
+
+	if (!m_pBuffer)
+#ifdef _WIN32
+		//    throw std::exception("Invalid array access: memory not allocated");
+		throw std::bad_alloc();
+#else
+		throw std::bad_alloc();
+#endif
+
+	memset(m_pBuffer, 0, r*c * sizeof(value_type));
+}
+
+template <typename T>
+void TArray2D<T>::resize(size_t _rows, size_t _cols)
+{
+	DeleteBuffer();
+	m_uRows = _rows;
+	m_uCols = _cols;
+
+	AllocateBuffer(m_uRows, m_uCols);
+}
+
+
+// row,col hash function (simple array index, column major form
+template <typename T>
+size_t TArray2D<T>::index_of(size_t r, size_t c)
+{
+	if (r >= m_uRows || c >= m_uCols)
+		throw invalid_index(r, c);
+	return  c + r * m_uCols;
+}
+
+// Array dimension accessors
+template <typename T>
+const size_t& TArray2D<T>::rows() const
+{
+	return m_uRows;
+}
+
+template <typename T>
+const size_t& TArray2D<T>::cols() const
+{
+	return m_uCols;
+}
+
+template <typename T>
+COORD2 TArray2D<T>::size() const
+{
+	COORD2 c(m_uRows, m_uCols);
+	return c;
+}
+
+// Element access methods
+template <typename T>
+T& TArray2D<T>::operator()(size_t row, size_t col) throw (std::bad_alloc)
+{
+	if (!m_pBuffer)
+#ifdef _WIN32
+		//    throw std::exception("Invalid array access: memory not allocated");
+		throw std::bad_array_new_length();
+#else
+		throw std::bad_alloc();
+#endif
+
+	return m_pBuffer[index_of(row, col)];
+}
+
+template <typename T>
+T  TArray2D<T>::operator()(size_t row, size_t col) const throw (std::bad_alloc)
+{
+	if (!m_pBuffer)
+#ifdef _WIN32
+		//    throw std::exception("Invalid array access: memory not allocated");
+		throw std::bad_array_new_length();
+#else
+		throw std::bad_alloc();
+#endif
+
+	return m_pBuffer[index_of(row, col)];
+}
+
+template <typename T>
+T& TArray2D<T>::operator()(COORD2 const & p) throw (std::bad_alloc)
+{
+	if (!m_pBuffer)
+#ifdef _WIN32    
+		//    throw std::exception("Invalid array access: memory not allocated");
+		throw std::bad_array_new_length();
+#else    
+		throw std::bad_alloc();
+#endif    
+
+	return m_pBuffer[index_of(p.row, p.col)];
+}
+
+template <typename T>
+T  TArray2D<T>::operator()(COORD2 const & p) const throw (std::bad_alloc)
+{
+	if (!m_pBuffer)
+#ifdef _WIN32
+		//    throw std::exception("Invalid array access: memory not allocated");
+		throw std::bad_array_new_length();
+#else
+		throw std::bad_alloc();
+#endif
+
+	return m_pBuffer[index_of(p.row, p.col)];
+}
+
+template <typename T>
+T* TArray2D<T>::buffer()
+{
+	return m_pBuffer;
+}
+
+template <typename T>
+void TArray2D<T>::clear()
+{
+	memset(m_pBuffer, 0, m_uRows*m_uCols * sizeof(T));
+}
+
+template <typename T>
+size_t const TArray2D<T>::length() const
+{
+	return m_uRows*m_uCols;
+}
+
+template <typename T>
+typename TArray2D<T>::iterator TArray2D<T>::begin()
+{
+	return m_pBuffer;
+}
+
+template <typename T>
+typename TArray2D<T>::const_iterator TArray2D<T>::begin() const
+{
+	return m_pBuffer;
+}
+
+template <typename T>
+typename TArray2D<T>::iterator TArray2D<T>::end()
+{
+	return m_pBuffer + length();
+}
+
+template <typename T>
+typename TArray2D<T>::const_iterator TArray2D<T>::end() const
+{
+	return m_pBuffer + length();
+}
+
+#endif
+
+// ************************************************************************** //
