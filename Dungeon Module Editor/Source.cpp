c79e4cdc5c938d3c0b9d824fa7510dc6b607be59
Thomas Walford <20815153+twalford@users.noreply.github.com>
2018-06-18 21:05:50 +1000

    Added support for new TELEPORTS

--- Dungeon Module Editor/Source.cpp
+++ Dungeon Module Editor/Source.cpp
@@ -31,7 +31,7 @@ private:
 		new olcSprite(L"dumoEditor_cropSouth.spr"),
 		new olcSprite(L"dumoEditor_cropWest.spr"),
 	};
-	olcSprite *mySprites[8] = {
+	olcSprite *mySprites[15] = {
 		new olcSprite(L"../OLCdungeon/sprites/dungeon_empty.spr"),
 		new olcSprite(L"../OLCdungeon/sprites/dungeon_stone.spr"),
 		new olcSprite(L"../OLCdungeon/sprites/dungeon_wall.spr"),
@@ -39,7 +39,14 @@ private:
 		new olcSprite(L"../OLCdungeon/sprites/dungeon_fire.spr"),
 		new olcSprite(L"../OLCdungeon/sprites/dungeon_hero.spr"),
 		new olcSprite(L"../OLCdungeon/sprites/dungeon_wood.spr"),
-		new olcSprite(L"../OLCdungeon/sprites/dungeon_web.spr")
+		new olcSprite(L"../OLCdungeon/sprites/dungeon_web.spr"),
+		new olcSprite(L"../OLCdungeon/sprites/dungeon_weakstone.spr"),
+		new olcSprite(L"../OLCdungeon/sprites/dungeon_portal_start_blue.spr"),
+		new olcSprite(L"../OLCdungeon/sprites/dungeon_portal_end_blue.spr"),
+		new olcSprite(L"../OLCdungeon/sprites/dungeon_portal_start_red.spr"),
+		new olcSprite(L"../OLCdungeon/sprites/dungeon_portal_end_red.spr"),
+		new olcSprite(L"../OLCdungeon/sprites/dungeon_portal_start_green.spr"),
+		new olcSprite(L"../OLCdungeon/sprites/dungeon_portal_end_green.spr")
 	};
 
 	DungeonModule *dm = nullptr;
@@ -49,7 +56,7 @@ protected:
 	virtual bool OnUserCreate()
 	{
 		dm = new DungeonModule(5, 5);
-		sCurrentModuleFile = L"../OLCdungeon/modules/dm_loops.dumo";
+		sCurrentModuleFile = L"../OLCdungeon/modules/dm_testing.dumo";
 
 		return true;
 	}
@@ -83,18 +90,18 @@ protected:
 			if (m_keys[0x35].bReleased) eCurrentENTITY = FIRE;
 			if (m_keys[0x36].bReleased) eCurrentENTITY = WOOD;
 			if (m_keys[0x37].bReleased) eCurrentENTITY = WEB;
-			//if (m_keys[0x38].bReleased) eCurrentENTITY = 
+			if (m_keys[0x38].bReleased) eCurrentENTITY = WEAKSTONE;
 			//if (m_keys[0x39].bReleased) eCurrentENTITY = 
 			//if (m_keys[0x30].bReleased) eCurrentENTITY = 
 		}
 		else if (nBrushSet == 2)
 		{
-			//if (m_keys[0x31].bReleased) eCurrentENTITY = 
-			//if (m_keys[0x32].bReleased) eCurrentENTITY = 
-			//if (m_keys[0x33].bReleased) eCurrentENTITY = 
-			//if (m_keys[0x34].bReleased) eCurrentENTITY = 
-			//if (m_keys[0x35].bReleased) eCurrentENTITY = 
-			//if (m_keys[0x36].bReleased) eCurrentENTITY = 
+			if (m_keys[0x31].bReleased) eCurrentENTITY = PORTAL_START_BLUE;
+			if (m_keys[0x32].bReleased) eCurrentENTITY = PORTAL_END_BLUE;
+			if (m_keys[0x33].bReleased) eCurrentENTITY = PORTAL_START_RED;
+			if (m_keys[0x34].bReleased) eCurrentENTITY = PORTAL_END_RED;
+			if (m_keys[0x35].bReleased) eCurrentENTITY = PORTAL_START_GREEN;
+			if (m_keys[0x36].bReleased) eCurrentENTITY = PORTAL_END_GREEN;
 			//if (m_keys[0x37].bReleased) eCurrentENTITY = 
 			//if (m_keys[0x38].bReleased) eCurrentENTITY = 
 			//if (m_keys[0x39].bReleased) eCurrentENTITY = 
@@ -184,12 +191,12 @@ protected:
 
 				if (m_keys[VK_SPACE].bReleased)
 				{
-					dm->SetTile(nPosX - 0, nPosY - 0, eCurrentENTITY);
+					dm->SetEntity(nPosX - 0, nPosY - 0, eCurrentENTITY);
 				}
 
 				if (m_keys[VK_DELETE].bReleased)
 				{
-					dm->SetTile(nPosX - 0, nPosY - 0, EMPTY);
+					dm->SetEntity(nPosX - 0, nPosY - 0, EMPTY);
 				}
 
 			}
@@ -212,13 +219,14 @@ protected:
 		DrawString(1, 1, L"F1-F4 = ENTITY SETS    F9 = Load File  F10 = Save File");
 		DrawString(1, 2, L"....");
 		Draw(nBrushSet, 2, L'o');
-		if		(nBrushSet == 1) DrawString(1, 3, L"1 = EMPTY  2 = FLOOR  3 = WALL   4 = ICE    5 = FIRE   6 = WOOD   7 = WEB    8 = ");
-		else if (nBrushSet == 2) DrawString(1, 3, L"1 =        2 =        3 =        4 =        5 =        6 =        7 =        8 = ");
+		if		(nBrushSet == 1) DrawString(1, 3, L"1 = EMPTY  2 = FLOOR  3 = WALL   4 = ICE    5 = FIRE   6 = WOOD   7 = WEB    8 = WEAK");
+		else if (nBrushSet == 2) DrawString(1, 3, L"1 = PTL BS 2 = PTL BE 3 = PTL RS 4 = PTL RE 5 = PTL GS 6 = PTL GE 7 =        8 = ");
 		else if (nBrushSet == 3) DrawString(1, 3, L"1 =        2 =        3 =        4 =        5 =        6 =        7 =        8 = ");
 		else if (nBrushSet == 4) DrawString(1, 3, L"1 =        2 =        3 =        4 =        5 =        6 =        7 =        8 = ");
 		
 		DrawString(1, 5, L"Current ENTITY:");
-		DrawSprite(1, 7, mySprites[eCurrentENTITY]);
+		//DrawSprite(1, 7, mySprites[eCurrentENTITY]);
+		DrawPartialSprite(1,7,mySprites[eCurrentENTITY],0,0,8,8);
 
 		// Draw Canvas
 		for (int x = 10; x < 118; x++)
@@ -245,12 +253,16 @@ protected:
 					if (x - nOffsetX < dm->width && y - nOffsetY < dm->height && x - nOffsetX >= 0 && y - nOffsetY >= 0)
 					{
 						// Draw Sprite
-						DrawSprite(x * nZoom + 11,
+						//DrawSprite(x * nZoom + 11,
+						//	y * nZoom + 10,
+						//	mySprites[dm->GetEntity(x - nOffsetX, y - nOffsetY)]);
+						DrawPartialSprite(x * nZoom + 11,
 							y * nZoom + 10,
-							mySprites[dm->GetTile(x - nOffsetX, y - nOffsetY)]);
+							mySprites[dm->GetEntity(x - nOffsetX, y - nOffsetY)],
+							0,0,8,8);
 
 						// Draw Pixel Markers
-						if (dm->GetTile(x - nOffsetX, y - nOffsetY) == EMPTY)
+						if (dm->GetEntity(x - nOffsetX, y - nOffsetY) == EMPTY)
 							Draw((x) * nZoom + 11, (y) * nZoom + 10, L'.');
 
 						// Draw Crop Markers
@@ -260,11 +272,13 @@ protected:
 							{
 								if (x == nPosX)
 									DrawSprite((x)* nZoom + 11, (y)* nZoom + 10, cropSprites[dInsertDirVert]);
+									//DrawPartialSprite((x)* nZoom + 11, (y)* nZoom + 10, cropSprites[dInsertDirVert], 0, 0, 8, 8);
 							}
 							else if (oCropOrientation == HORIZONTAL)
 							{
 								if (y == nPosY)
 									DrawSprite((x)* nZoom + 11, (y)* nZoom + 10, cropSprites[dInsertDirHori]);
+									//DrawPartialSprite((x)* nZoom + 11, (y)* nZoom + 10, cropSprites[dInsertDirHori], 0, 0, 8, 8);
 									
 							}
 						}
